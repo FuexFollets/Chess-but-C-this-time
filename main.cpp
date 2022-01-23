3a7290c4381709a5e542441ebb5562e866b67e7b
Fuex Follets <85237518+FuexFollets@users.noreply.github.com>
2022-01-23 16:01:51 +0000

    Debugged compiling issues and added node class and functions. TODO: add multithreading to tree evaluation and debug

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@
 #include <map>
 #include <array>
 #include <cstdlib>
-#include <memory>
 #include <stdexcept>
-#include <unordered_set>
-#include <set>
+#include <unordered_map>
+#include <thread>
 
 // definitions, data, and primitive functions
- 
+
 class exec_outer_main {
   public:
     exec_outer_main(void (*exfn)()) { exfn(); }
 };
- 
+
+template <typename Con> class map_set{
+  private:
+    int appen{0};
+  public:
+    std::unordered_map<int, Con> as_map;  
+    void append(Con obj) {
+      this->as_map[appen++] = obj;
+    }
+    void append_set(map_set<Con> ap_set) {
+      for (const auto&[_, obj]: ap_set) {
+        append(obj);
+      }
+    }
+
+    inline auto begin() { return as_map.begin(); }
+    inline auto end() { return as_map.end(); }
+    inline bool empty() { return as_map.empty(); }
+    inline void erase(int key) { as_map.erase(key); }
+};
+
+
 template <typename I1, typename I2> std::map<I1, I2> zip2map(const std::vector<I1>& iter1, const std::vector<I2>& iter2) {
   std::map<I1, I2> zipped;
   int inst{};
@@ -83,6 +103,10 @@ struct move_type {
   int new_y; int new_x;
   move_type(int x_=0, int y_=0, int new_y_=0, int new_x_=0) :
     y{y_}, x{x_}, new_y{new_y_}, new_x{new_x_} {}
+
+  friend bool operator==(move_type m1, move_type m2) {
+    return ((m1.y == m2.y) && (m1.x == m2.x) && (m1.new_y == m2.new_y) && (m1.new_x == m2.new_x));
+  }
 };
 
 std::vector<move_type> blank_move_list{};
@@ -100,7 +124,9 @@ inline int get_color(const int id) { return static_cast<int>(id > 7); }
 inline int get_type(const int id) { return ((id - 1) % 6) + 1; }
  
 inline int get_dir(const int turn) { return (turn * (-2) + 1); }    
- 
+
+inline int get_c(const int color) { return color * 2 - 1; }
+
 b88 from_pgn(const std::string_view pgn) {
   b88 b{zero_board};
   char ex_pgn[72]; int sel{0};
@@ -169,7 +195,26 @@ m_init initialize_diff() {
 const auto mov_s{initialize_diff()};
  
 // board methods
+
+class C_board {
+  public:
+    C_board(int t = 1, b88 set_board = initial_board, std::vector<move_type> moves = blank_move_list) {
+      turn = t;
+      pos = set_board;
+      move_arr = moves;
+    }
  
+    b88 pos{};
+    int turn{};
+    std::vector<move_type> move_arr{};
+
+    inline friend bool operator==(C_board b1, C_board b2) {
+    return ((b1.turn == b2.turn) && (b1.pos == b2.pos) && (b1.move_arr == b2.move_arr));
+    }
+
+
+};
+
 b88 after_move(const b88 b, const move_type m, const int turn) {
   b88 new_b{b};
  
@@ -188,7 +233,11 @@ b88 after_move(const b88 b, const move_type m, const int turn) {
   }
   return new_b;
 }
- 
+
+inline C_board cboard_after_move(C_board cb, move_type m) {
+  return C_board(1 - cb.turn, after_move(cb.pos, m, cb.turn), cb.move_arr);
+}
+
 int has_king(const b88 board, const int color) {
   for (const std::array<int, 8>& row: board) {
     for (const int& id: row) {
@@ -211,44 +260,44 @@ std::array<int, 2> king_loc(const b88 board, int color) {
   return std::array<int, 2>{-1, -1};
 }
  
-std::set<move_type> square_moves(const b88 board, const int y, const int x, const std::vector<move_type> past_moves = blank_move_list) {
+map_set<move_type> square_moves(const b88 board, const int y, const int x, const std::vector<move_type> past_moves = blank_move_list) {
   auto last_move{past_moves[0]};
   int piece{board[y][x]}; int type{get_type(piece)}; int color{get_color(piece)};
-  std::set<move_type> valid_moves;
+  map_set<move_type> valid_moves;
   if (type == 1) { // if pawn
     int dir{get_dir(color)};
     if (!board[y + dir][x]) { //moving forward
-      valid_moves.insert(move_type{y, x, y + dir, x});
+      valid_moves.append(move_type{y, x, y + dir, x});
       if ((!(board[y + 2 * dir][x])) && (!(x - (-3) * dir) + 5)){
-        valid_moves.insert(move_type{y, x, y + 2 * dir, x});
+        valid_moves.append(move_type{y, x, y + 2 * dir, x});
       }
     }
     for (int xd{-1}; xd < 2; xd++) { // taking pieces
         if (((x + xd) < 8) && (x + xd) > -1) {
           if ((get_color(board[y + dir][x + xd]) ^ color) && (!(board[y + dir][x + xd]))) {
-            valid_moves.insert(move_type{y, x, y + dir, x + xd});
+            valid_moves.append(move_type{y, x, y + dir, x + xd});
           }
         }
       }
     if (last_move.y - 11) {
       if ((get_type(board[last_move.new_y][last_move.new_x]) == 1) && ((last_move.y + 2 * dir) == ((last_move.new_y)) && (y == last_move.new_y) && (std::abs(last_move.new_x) - x) == 1)) {
-        valid_moves.insert(move_type{y, x, y + dir, last_move.new_x});
+        valid_moves.append(move_type{y, x, y + dir, last_move.new_x});
       }  
     }
   } else if ((type == 2) || (type == 6)) { // if king or knight
     const std::vector<m_diff>& space_iter{(mov_s.m1).at(type)};
     for (const auto&[yd, xd]: space_iter) {
       int space{board[y + yd][x + xd]};
-      if (!(space) || (color ^ get_color(space))) { valid_moves.insert(move_type{y, x, y + yd, x + xd}); }
+      if (!(space) || (color ^ get_color(space))) { valid_moves.append(move_type{y, x, y + yd, x + xd}); }
     }
   } else {
     const auto& space_iter{(mov_s.m2).at(type)};
     for (const auto& direction: space_iter) {
       for (const auto&[yd, xd]: direction) {  
         int space{board[y + yd][x + xd]};
-        if (!space) { valid_moves.insert(move_type{y, x, y + yd, x + xd}); }
+        if (!space) { valid_moves.append(move_type{y, x, y + yd, x + xd}); }
         else if (get_color(space) ^ color) {
-          valid_moves.insert(move_type{y, x, y + yd, x + xd});
+          valid_moves.append(move_type{y, x, y + yd, x + xd});
           break;
         } else { break; }
       }
@@ -260,8 +309,8 @@ std::set<move_type> square_moves(const b88 board, const int y, const int x, cons
 int is_cont(const b88 board, const int y, const int x, const int turn) {
   for (int sel_y{}; sel_y < 8; sel_y++) {
     for (int sel_x{}; sel_x < 8; sel_x++) {
-      auto opm{square_moves(board, y, x)};
-      for (const auto& move: opm) {
+      auto opm{square_moves(board, y, x).as_map};
+      for (const auto&[_, move]: opm) {
         if ((move.new_y == y) && (move.new_x == x)) {
           return 1;
         }
@@ -271,14 +320,14 @@ int is_cont(const b88 board, const int y, const int x, const int turn) {
   return 0;
 }
  
-std::set<move_type> av_moves(const b88 board, const std::vector<move_type> past_moves, const int color) {
-  std::set<move_type> moves;
+map_set<move_type> av_moves(const b88 board, const std::vector<move_type> past_moves, const int color) {
+  map_set<move_type> moves;
   for (int y{}; y < 8; y++) {
     for (int x{}; x < 8; x++) {
       if (board[y][x]) {
         if (get_color(board[y][x]) == color) {
           auto m_for_square{square_moves(board, y, x, past_moves)};
-          moves.insert(m_for_square.begin(), m_for_square.end());
+          moves.append_set(m_for_square);
         }
       }
     }
@@ -290,7 +339,7 @@ std::set<move_type> av_moves(const b88 board, const std::vector<move_type> past_
         break;
       } else if (space) {  
         int h{(side + 25)/3};
-        moves.insert(move_type{h, h, h, h});
+        moves.append(move_type{h, h, h, h});
       }
     }
   }
@@ -302,29 +351,40 @@ int is_legal_pos(b88 board, int color) {
   return is_cont(board, kl[0], kl[1], color);
 }
  
-std::set<move_type> legal_moves(const b88 board, const int turn, const std::vector<move_type> past_moves = blank_move_list) {
-  auto vm{av_moves(board, past_moves, turn)};
-  std::set<move_type> lg_moves;
-  for (const move_type& move: vm) {
+map_set<move_type> legal_moves(const b88 board, const int turn, const std::vector<move_type> past_moves = blank_move_list) {
+  auto vm{av_moves(board, past_moves, turn).as_map};
+  map_set<move_type> lg_moves;
+  for (const auto&[_, move]: vm) {
     if (is_legal_pos(after_move(board, move, turn), turn)) {
-      lg_moves.insert(move);
+      lg_moves.append(move);
     }
   }
   return lg_moves;
 }
- 
-class C_board {
-  public:
-    C_board(int t = 1, b88 set_board = initial_board, std::vector<move_type> moves = blank_move_list) {
-      turn = t;
-      pos = set_board;
-      move_arr = moves;
+
+map_set<move_type> legal_moves(const C_board cb) {
+  const auto board{cb.pos}; const auto past_moves{cb.move_arr}; const auto turn{cb.turn};
+  auto vm{av_moves(board, past_moves, turn).as_map};
+  map_set<move_type> lg_moves;
+  for (const auto&[_, move]: vm) {
+    if (is_legal_pos(after_move(board, move, turn), turn)) {
+      lg_moves.append(move);
     }
- 
-    b88 pos{};
-    int turn{};
-    std::vector<move_type> move_arr{};
-};
+  }
+  return lg_moves;
+}
+
+int count_ID(const b88 board, const int id) {
+  int count{0};
+  for (const auto& row: board) {
+    for (const int& val: row) {
+      if (!(val - id)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
 
 struct init_fl{
   float val;
@@ -335,79 +395,122 @@ struct init_fl{
 
 class C_node {
   public:
-    static int n_count;
- 
-    C_node(C_board board) {
-      n_count++;
+    C_node(C_board board, int* n_count_inp) {
       node_board = board;
+      node_count = n_count_inp;
+      (*node_count)++;
     }
  
     ~C_node() {
-      n_count--;
-      for (C_node* node_ptr: branches) { delete node_ptr; }
+      (*node_count)--;
+      for (auto&[ind, node_ptr]: branches.as_map) { delete node_ptr; }
     }
  
     C_board node_board;
     init_fl eval;
-    std::set<C_node*> branches;
+    map_set<C_node*> branches;
+    int& turn{node_board.turn};
+    int* node_count;
    
-    inline void add_branch(C_node* b_ptr) { branches.insert(b_ptr); }
+    inline void add_branch(C_node* new_node_ptr) { branches.append(new_node_ptr); }
  
 };
-/*
-void get_leaves(const C_node* h_node, std::set<C_node*> appen_s) {
-  if (h_node->branches.empty()) {
-    appen_s.insert(h_node);
-  } else {
 
+// Z0 (zero depth) evaluation functions //
+
+float surface_eval(b88 board) { return 0.0F; }
+
+bool trunicate() { return false; }
+
+// //
+
+float min_max_eval(const C_node* h_node, int min_max) { // 1=max -1=min
+  float mm{0.0F};
+  for (const auto[_, node]: (h_node->branches.as_map)) {
+    if (mm < (node->eval.val) * min_max) { mm = node->eval.val; }
   }
+  return mm;
 }
-*/
-int count_ID(const b88 board, const int id) {
-  int count{0};
-  for (const auto& row: board) {
-    for (const int& val: row) {
-      if (!(val - id)) {
-        count++;
-      }
-    }
-  }
-}
-
-float surface_eval(b88 board) {
-  return 0.0F;
-}
-
-bool trunicate() { return true; }
 
 void set_depth_ev(C_node* h_node) {
-  if ((h_node->branches).empty()) {
+  if (h_node->branches.empty() && h_node->eval.init) {
     h_node->eval = init_fl{surface_eval(h_node->node_board.pos), true};
   } else {
-    if ((*((h_node->branches).begin()))->eval.init) { // if the branches are initialized
-      for (C_node* node: h_node->branches) { set_depth_ev(node); }
+    if (!h_node->branches.as_map[0]->eval.init) {
+      for (auto&[_, node_ptr]: h_node->branches) { set_depth_ev(h_node); }
     }
-
-
-    
+    h_node->eval = init_fl{min_max_eval(h_node, get_dir(h_node->turn)), true};
   }
 }
 
-void un_init(C_node& h_node) {
-  h_node.eval.init = false;
-  if (!h_node.branches.empty()) {
-    for (C_node& node: h_node.branches) {
+void form_branches(C_node* h_node) {
+  if (h_node->branches.empty()) {
+    auto moves{legal_moves(h_node->node_board)}; 
+    for (const auto&[_, move]: moves) {
+      if (!trunicate()) {
+        h_node->add_branch(new C_node(cboard_after_move(h_node->node_board, move), h_node->node_count));
+      }
+    }
+  } else {
+    for (const auto&[_, node]: h_node->branches) {
+      form_branches(node);
+    }
+  }
+}
+
+void un_init(C_node* h_node) {
+  h_node->eval.init = false;
+  if (!h_node->branches.empty()) {
+    for (auto[_, node]: h_node->branches) {
       un_init(node);
     }
   }
 }
 
+bool compare_cboard(C_board b1, C_board b2) {
+  return ((b1.turn == b2.turn) && (b1.pos == b2.pos) && (b1.move_arr == b2.move_arr));
+}
 
- 
+void tree_move(C_node* h_node, const move_type move) {
+  const auto pos{cboard_after_move(h_node->node_board, move)}; bool has_branch{false};
+  for (auto[key, node]: h_node->branches) {
+    if (node->node_board == pos) {
+      has_branch = true;
+      h_node->branches.erase(key);
+      delete h_node;
+      h_node = node;
+      un_init(h_node);
+      break;
+    }
+  }
+  if (!has_branch) {
+    delete h_node;
+    h_node->add_branch(new C_node(pos, h_node->node_count));
+  }
+}
+
+class Evaluation_thread {
+  public:
+    Evaluation_thread(C_board cb) {
+      main_node = new C_node(cb, &total_nodes);
+    }
+
+    void inf_evaluation() {}
+    void inf_forming() {}
+
+    void make_move(move_type move) {
+      tree_move(main_node, move);
+    }
+
+    int total_nodes;
+  private:
+    C_node* main_node;
+    bool is_calc{false};
+};
+
 int main() {
-    std::cout << "no errors\n";
- 
-   
- 
-    return 0;
+
+  
+  std::cout << "Compilation Sucessful\n";
+  return 0;
 }
